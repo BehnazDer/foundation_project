2e838f31840cc59047f0264ed918f6b96950d3b0
Behnaz Derakhshani <bderakhshani@CT-Guest.local>
2019-04-18 12:41:32 +0200

    second Assessment

--- auth.py
+++ auth.py
@@ -1,39 +1,58 @@
-from flask import Flask, render_template, redirect, request, url_for, Blueprint
+from flask import Flask, render_template, redirect, request, url_for, flash
 from database import get_db
 import sqlite3
 app = Flask(__name__) #creating the application object
+app.secret_key = 41003
 DATABASE='mydatabase.db'
 
 @app.route('/') # @ is a decorator and links the function to the url
 def home():
-    return "Welcome to the PawClubs!"
-bp = Blueprint('auth', __name__, url_prefix='/auth')
- #Flask associates view functions wih bluprints when dispatching requests & generating URLs from one endpoint to another
+    return render_template('auth/home.html')
+
 @app.route('/login', methods=('GET', 'POST')) # by default flask assumes that all methods are GET
 def login():  
-    if request.method == 'POST':
+    if  request.method == 'POST':
         user_username = request.form['username']
         user_password = request.form['password']
         print(user_username, user_password)
         if not user_username or not user_password:
-            return render_template('auth/login.html')
+            return render_template('auth/home.html')
         #to get data from database
         cursor = get_db()
-        cursor.execute("SELECT username, password  FROM users WHERE username= '{}' and password='{}'" .format(user_username,user_password))
+        cursor.execute("SELECT username, password  FROM users WHERE username= '{}' and password='{}'".format(user_username,user_password))
         user_info = cursor.fetchall()
         print(user_info)
-        return redirect("/")
+        return render_template('auth/home.html')
+        #return redirect("/") #which return??
     else:
         return render_template('auth/login.html')
 
-@bp.route('/register', methods=('GET', 'POST'))
+@app.route('/register', methods=('GET', 'POST'))
 def register():
-    # if request.method == 'POST':
-    #     user_username = request.form['username']
-    #     user_password = request.form['password']
-    # else:
-    #     return redirect(url_for('home'))
-    return render_template('auth/register.html')
+    if request.method == 'POST':
+       user_name = request.form['name']
+       user_email = request.form['email']
+       user_username = request.form['username']
+       user_password = request.form['password']
+       user_repeatpassword = request.form['repeatpassword']
+       cursor = get_db()
+       error = None
+    if not user_name or not user_email or not user_username or not user_password or not user_repeatpassword:
+        return render_template('auth/register.html', error= "This information is required")
+    elif user_password != user_repeatpassword:
+        return render_template('auth/register.html', error= "Invalid Password")
+         
+    elif cursor.execute('SELECT ID FROM users WHERE email = ? ', (user_email,)).fetchone() is not None:
+        error = 'User {} is already registered.'.format(user_email)
+    if error is None:   
+        cursor.execute("INSERT INTO users (name, email, username, password, repeatpassword) values(?, ?, ?, ?, ?)",(user_name, user_email, 
+                        user_username, user_password, user_repeatpassword))
+        
+        user_info = cursor.fetchall()
+  
+        return render_template('auth/home.html')
+    else:
+        return render_template('auth/register.html')
 
 
 
