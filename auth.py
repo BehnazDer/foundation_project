2bdd57aceaa893bfb9014ab032034567003eb1ac
Behnaz Derakhshani <bderakhshani@CT-Guest.home>
2019-04-28 23:45:55 +0200

    3rd assessment

--- auth.py
+++ auth.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, redirect, request, url_for, flash
+from flask import Flask, render_template, redirect, request, url_for
 from database import get_db
 import sqlite3
 app = Flask(__name__) #creating the application object
 app.secret_key = 41003
 DATABASE='mydatabase.db'
 
-@app.route('/') # @ is a decorator and links the function to the url
+
+@app.route('/')
+def landing():
+    return render_template('auth/landing.html')
+@app.route('/home') # @ is a decorator and links the function to the url
 def home():
     return render_template('auth/home.html')
 
@@ -16,41 +20,44 @@ def login():
         user_password = request.form['password']
         print(user_username, user_password)
         if not user_username or not user_password:
-            return render_template('auth/home.html')
+            return render_template('auth/login.html')
         #to get data from database
-        cursor = get_db()
+        cursor, connection = get_db()
         cursor.execute("SELECT username, password  FROM users WHERE username= '{}' and password='{}'".format(user_username,user_password))
         user_info = cursor.fetchall()
-        print(user_info)
-        return render_template('auth/home.html')
-        #return redirect("/") #which return??
+        if user_info:
+            return redirect('/home')
+        else:
+            return render_template('auth/login.html', error= "This information is required")
+           
     else:
         return render_template('auth/login.html')
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
-       user_name = request.form['name']
-       user_email = request.form['email']
-       user_username = request.form['username']
-       user_password = request.form['password']
-       user_repeatpassword = request.form['repeatpassword']
-       cursor = get_db()
-       error = None
-    if not user_name or not user_email or not user_username or not user_password or not user_repeatpassword:
-        return render_template('auth/register.html', error= "This information is required")
-    elif user_password != user_repeatpassword:
-        return render_template('auth/register.html', error= "Invalid Password")
-         
-    elif cursor.execute('SELECT id FROM users WHERE email = ? ', (user_email,)).fetchone() is not None:
-        error = 'User {} is already registered.'.format(user_email)
-    if error is None:   
-        cursor.execute("INSERT INTO users (name, email, username, password, repeatpassword) values(?, ?, ?, ?, ?)",(user_name, user_email, 
-                        user_username, user_password, user_repeatpassword))
-        
-        user_info = cursor.fetchall()
-  
-        return render_template('auth/home.html')
+        user_name = request.form['name']
+        user_email = request.form['email']
+        user_username = request.form['username']
+        user_password = request.form['password']
+        user_repeatpassword = request.form['repeatpassword']
+        cursor, connection = get_db()
+        error = None
+        if not user_name or not user_email or not user_username or not user_password or not user_repeatpassword:
+            return render_template('auth/register.html', error= "This information is required")
+        elif user_password != user_repeatpassword:
+            return render_template('auth/register.html', error= "Invalid Password")
+            
+        elif cursor.execute('SELECT id FROM users WHERE email = ? ', (user_email,)).fetchone() is not None:
+            error = 'User {} is already registered.'.format(user_email)
+        if error is None:   
+            cursor.execute("INSERT INTO users (name, email, username, password, repeatpassword) values(?, ?, ?, ?, ?)",(user_name, user_email, 
+                            user_username, user_password, user_repeatpassword))
+
+            # insert_into_db("INSERT ...")
+            # insertUser(user_username, user_password)
+            connection.commit()
+            return redirect('/home')
     else:
         return render_template('auth/register.html')
 
--- database.py
+++ database.py
@@ -1,13 +1,20 @@
 import sqlite3 as sql
 
-def insertUser(username,password):
+def insertUser(user_name, user_email, user_username,user_password, user_repeatpassword):
     con = sql.connect("mydatabase.db")
     cur = con.cursor()
-    cur.execute("INSERT INTO users (username,password) VALUES (?,?)", (username,password))
+    cur.execute("INSERT INTO users (name,email,username,password,repeatpassword) VALUES (?,?)", (user_name, user_email, user_username,user_password, user_repeatpassword))
     con.commit()
     con.close()
 
 def get_db():
     con = sql.connect("mydatabase.db")
     cur = con.cursor()
-    return cur
+    return cur, con
+
+def insert_into_db(command):
+    con = sql.connect("mydatabase.db")
+    cur = con.cursor()
+    cur.execute(command)
+    con.commit()
+    con.close()
